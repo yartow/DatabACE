424a3cc52a6da5eb69aa1211e7f55674587fdc5d
yartow <yartow@users.noreply.github.com>
2024-12-23 10:43:50 +0100

    Update from Windows

--- 03. Code/02. SQL/Views/vw_Person.sql
+++ 03. Code/02. SQL/Views/vw_Person.sql
@@ -1,10 +1,12 @@
+create view vw_Person as 
+
 select 
     p.Surname, 
     p.CallName, 
     p.DateOfBirth, 
     cl.Class, 
-    l.FirstLanguage, 
-    l.SecondLanguage, 
+    l.language as FirstLanguage, 
+    l2.language as SecondLanguage, 
     b.Baptism, 
     d.Denomination,
 	p.IsDislectic
@@ -13,15 +15,29 @@ join Address a
     on p.AddressID = a.ID
 join Class cl 
     on cl.id = p.classId
-join Language l 
+left join Language l 
     on l.id = p.FirstLanguageID
-join language l2 
+left join language l2 
     on l2.id = p.SecondLanguageID
 join baptism b 
     on b.id = p.BaptismId
 join Denomination d 
     on d.id = p.denominationID
+join State s 
+    on s.id = p.StateID
 where 1=1 
     and p.[Status] = 'Student'
-    and s.State = 'Active'
+    and s.State = 'Enrolled'
 ;
+
+
+
+/*
+Tables to create: 
+Address
+Class
+Language
+Baptism
+Denomination
+State
+*/
